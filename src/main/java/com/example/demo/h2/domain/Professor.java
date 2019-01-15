2284bfe551126285495b8cc38509f947dfbc11c1
Jasmeet Singh <jasmeet.singh@staples.com>
2019-01-15 18:03:50 -0500

    Prof added

---
+++ src/main/java/com/example/demo/h2/domain/Professor.java
@@ -0,0 +1,28 @@
+package com.example.demo.h2.domain;
+
+import java.util.List;
+
+public class Professor {
+
+	private String name;
+	private Long id;
+	private List<Student> students;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+}
